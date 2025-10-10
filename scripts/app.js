28f0c9005fbb6ef6dedec627c7e60e75d308062f
Jonas Olson <jonas.olson@gmail.com>
2025-10-10 16:21:32 +0200

    Till och med lektion 108. Börjat med UI:t. Lade till ikonfilerna

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,31 @@
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+
+const updateUI = (data) => {
+  //   const cityDets = data.cityDets;
+  //   const weather = data.weather;
+
+  // Destructure properties. Gör samma sak som ovan.
+  const { cityDets, weather } = data;
+
+  //update details template
+  details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my 4">
+          <span>${weather.Temperature.Metric.Value}</span>
+          <span>&deg;C</span>
+        </div>
+  `;
+
+  //remove d-none if present
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 
 const updateCity = async (city) => {
   const cityDets = await getCity(city);
@@ -19,6 +46,6 @@ cityForm.addEventListener("submit", (e) => {
 
   //Update UI with the new city
   updateCity(city)
-    .then((data) => console.log(data))
+    .then((data) => updateUI(data))
     .catch((err) => console.log(err));
 });
