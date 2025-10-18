fd7880ed3ce74420f06daa362c54c1f8945d25a1
Jonas Olson <jonas.olson@gmail.com>
2025-10-18 21:04:44 +0200

    Went back to cards design.

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,9 @@ const card = document.querySelector(".card");
 const details = document.querySelector(".details");
 const forecastContainer = document.querySelector(".forecast-container");
 
-const time = document.querySelector("img.time");
-const icon = document.querySelector(".icon img");
+const backgroundImg = document.querySelector("img.time");
+const icon = document.querySelector(".icon");
+const iconImg = document.querySelector(".icon img");
 
 const locationContainer = document.querySelector(".locationContainer");
 const currentWeatherContainer = document.querySelector(
@@ -22,50 +23,59 @@ const updateUI = (data) => {
   const timestamp = weather.dt;
   const timezone = weather.timezone;
   let localTime = new Date(timestamp * 1000 + timezone * 1000);
-  const localHours = localTime.getUTCHours();
+  const localHour = localTime.getUTCHours();
   //Location
-  locationContainer.innerHTML = `<h3 class="my-3">${cityDets.name}</h3>`;
+  /*  locationContainer.innerHTML = `<h3 class="my-3">${cityDets.name}</h3>`;
   //Current weather
 
   currentWeatherContainer.innerHTML += `
-  <p>Kl. ${localHours}</p>
-  <span>${Math.floor(weather.main.temp)}&deg;</span>
-  Känns som ${Math.floor(weather.main.feels_like)}&deg;
-  `;
+  <p>Kl. ${localHour}</p>
+  <span>${Math.round(weather.main.temp)}&deg;</span>
+  Känns som ${Math.round(weather.main.feels_like)}&deg;
+  `; */
 
   //update details template
   details.innerHTML = `
         <h5 class="my-3">${cityDets.name}</h5>
+          <p>Kl. ${localHour}</p>
         <div class="my-3">${weather.weather[0].description}</div>
         <div class="display-4 my 4">
-          <span>${Math.floor(weather.main.temp)}</span>
+          <span>${Math.round(weather.main.temp)}</span>
           <span>&deg;C</span>
         </div>`;
   // 5 day forecast presentation
   forecastContainer.innerHTML = `
   <h5 class="my-3">5 day </h5>
   <p>${forecast.list[0].dt_txt}</p>
-  <p>Temp: ${Math.floor(forecast.list[0].main.temp)}</p>
+  <p>Temp: ${Math.round(forecast.list[0].main.temp)}</p>
+  <p>Känns som ${Math.round(forecast.list[0].main.feels_like)}</p>
   <p>% chance for rain ${forecast.list[0].pop}</p>
   <p>mm of rain ${forecast.list[0].rain}</p>
+    <p>${forecast.list[8].dt_txt}</p>
+  <p>Temp: ${Math.round(forecast.list[8].main.temp)}</p>
+    <p>${forecast.list[16].dt_txt}</p>
+  <p>Temp: ${Math.round(forecast.list[16].main.temp)}</p>
+      <p>${forecast.list[24].dt_txt}</p>
+  <p>Temp: ${Math.round(forecast.list[24].main.temp)}</p>
+    <p>${forecast.list[32].dt_txt}</p> 
  
   `;
   /* 
   <p>${forecast.list[8].dt_txt}</p>
-  <p>Temp: ${Math.floor(forecast.list[8].main.temp)}</p>
+  <p>Temp: ${Math.round(forecast.list[8].main.temp)}</p>
     <p>${forecast.list[16].dt_txt}</p>
-  <p>Temp: ${Math.floor(forecast.list[16].main.temp)}</p>
+  <p>Temp: ${Math.round(forecast.list[16].main.temp)}</p>
       <p>${forecast.list[24].dt_txt}</p>
-  <p>Temp: ${Math.floor(forecast.list[24].main.temp)}</p>
+  <p>Temp: ${Math.round(forecast.list[24].main.temp)}</p>
     <p>${forecast.list[32].dt_txt}</p> emp)}</p> 
  */
 
   //update icon images
   const iconSrc = `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
   console.log("iconSrc: ", iconSrc);
-  icon.setAttribute("src", iconSrc);
+  iconImg.setAttribute("src", iconSrc);
   //update night/day background
-  let timeSrc = Math.floor(Date.now() / 1000);
+  let timeSrc = Math.round(Date.now() / 1000);
   console.log("Local time ", timeSrc);
   console.log("Sunrise", weather.sys.sunrise);
   timeSrc =
@@ -73,7 +83,7 @@ const updateUI = (data) => {
       ? "img/day.svg"
       : "img/night.svg";
 
-  time.setAttribute("src", timeSrc);
+  backgroundImg.setAttribute("src", timeSrc);
 
   //remove d-none if present
   if (card.classList.contains("d-none")) {
