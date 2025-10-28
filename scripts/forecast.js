755004e25d5962ab555ddde072dffbc0af022ea8
Jonas Olson <jonas.olson@gmail.com>
2025-10-28 20:30:35 +0100

    Added API_key all over for Vercel

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -10,7 +10,6 @@ export {
 // import { key } from "./api.js";
 const API_KEY = process.env.API_KEY;
 
-const key = API_KEY;
 //Get weather information
 
 //Set Metric or Imperial units
@@ -29,7 +28,7 @@ const getLang = () => lang;
 const getWeather = async (lat, lon) => {
   // const key = process.env.WEATHER_API_KEY;
   const base = `https://api.openweathermap.org/data/2.5/weather?`;
-  const query = `lat=${lat}&lon=${lon}&units=${units}&appid=${key}&lang=${lang}`;
+  const query = `lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}&lang=${lang}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
@@ -40,7 +39,7 @@ const getWeather = async (lat, lon) => {
 // Get 5-day forecast
 const getForecast = async (lat, lon) => {
   const base = `https://api.openweathermap.org/data/2.5/forecast?`;
-  const query = `lat=${lat}&lon=${lon}&units=${units}&appid=${key}&lang=${lang}`;
+  const query = `lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}&lang=${lang}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
@@ -53,7 +52,7 @@ const getForecast = async (lat, lon) => {
 //Get city information. Gets Key that is then used in getWeather to get the weather for that city.
 const getCity = async (city) => {
   const base = "http://api.openweathermap.org/geo/1.0/direct";
-  const query = `?q=${city}&limit=1&appid=${key}&lang=${lang}`;
+  const query = `?q=${city}&limit=1&appid=${API_KEY}&lang=${lang}`;
 
   const response = await fetch(base + query);
   const data = await response.json();
